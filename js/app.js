6056b07469bc18da13d94f568b0e3d79f2a8e96a
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-01 12:56:31 -0400

    portfolio webpage new project

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*=====================
+RETRIEVE ELEMENTS
+======================*/
+const projectTitle = document.querySelectorAll(".project-title");
+const code = document.querySelectorAll(".code");
+
+/*=====================
+  
+======================*/
+// projectTitle.forEach(e => {
+//     e.addEventListener("click", () => {
+//         code.forEach(e => {
+//             e.classList.add = "vis"
+//          });
+//      });
+    
+// });
+
+/*=====================
+  
+======================*/
+
+/*=====================
+  
+======================*/
